9da9c3d7b021663d7dc095a8987765fca0df1c7c
Muhammed Amir T <muhammedamirt@gmail.com>
2023-02-04 21:55:21 +0530

    cors fixing

--- build/server.js
+++ build/server.js
@@ -29,7 +29,7 @@ app.use(body_parser_1.default.urlencoded({ extended: false }));
 // app.use(cors())
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.woulddo.iworldecart.shop');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.woulddo.iworldecart.shop/');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@
 import dotenv from 'dotenv'
 import express, { Application } from 'express'
 import mongoose from 'mongoose'
-// import cors from 'cors'
+import cors from 'cors'
 import userRoute from './routes/user'
 import adminRoute from './routes/admin'
 import chatRoute from './routes/chatRoute'
@@ -17,27 +17,27 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// app.use(cors({
-//   origin: ['https://www.woulddo.iworldecart.shop/'],
-//   methods: ['GET', 'POST', 'DELETE', 'PUT'],
-//   credentials: true
-// }))
+app.use(cors({
+  origin: ['https://www.woulddo.iworldecart.shop/'],
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  credentials: true
+}))
 
 // app.use(cors())
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.woulddo.iworldecart.shop')
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', 'https://www.woulddo.iworldecart.shop/')
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
 
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-  // Pass to next layer of middleware
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+//   // Pass to next layer of middleware
+//   res.setHeader('Access-Control-Allow-Credentials', 'true')
 
-  next()
-})
+//   next()
+// })
 
 app.use('/', userRoute)
 app.use('/admin', adminRoute)
