adf1503f5ec8e0724461dfc3f1cdc59444bb691a
Muhammed Amir T <muhammedamirt@gmail.com>
2023-02-05 13:00:35 +0530

    cors

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@
 import dotenv from 'dotenv'
 import express, { Application } from 'express'
 import mongoose from 'mongoose'
-// import cors from 'cors'
+import cors from 'cors'
 import userRoute from './routes/user'
 import adminRoute from './routes/admin'
 import chatRoute from './routes/chatRoute'
@@ -23,19 +23,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //   credentials: true
 // }))
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.woulddo.iworldecart.shop')
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', 'https://www.woulddo.iworldecart.shop')
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
 
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-  // Pass to next layer of middleware
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+//   // Pass to next layer of middleware
+//   res.setHeader('Access-Control-Allow-Credentials', 'true')
 
-  next()
-})
+//   next()
+// })
+
+app.use(cors())
 
 app.use('/', userRoute)
 app.use('/admin', adminRoute)
