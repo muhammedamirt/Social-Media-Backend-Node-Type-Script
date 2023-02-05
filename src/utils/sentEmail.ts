77e1af9c45a8730a75123540872ca8d40755ab00
Muhammed Amir T <muhammedamirt@gmail.com>
2023-02-05 22:34:48 +0530

    node mailer error

--- src/utils/sentEmail.ts
+++ src/utils/sentEmail.ts
@@ -2,20 +2,35 @@ import { config } from 'dotenv'
 config()
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const nodemailer = require('nodemailer')
+// const { google } = require('googleapis');
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const sendEmail = async (email: string, subject: string, text: string) => {
   try {
+    // const transporter = nodemailer.createTransport({
+    //   host: process.env.HOST,
+    //   service: process.env.SERVICE,
+    //   port: 587,
+    //   secure: true,
+    //   auth: {
+    //     user: process.env.USER,
+    //     pass: process.env.PASS
+    //   }
+    // })
+
+    // const oauth2Client = new google.auth.OAuth2(
+    //   process.env.GOOGLE_CLIENT_ID,
+    //   process.env.GOOGLE_CLIENT_SECRET_ID,
+    //   'https://www.wouldoback.iworldecart.shop'
+    // )
     const transporter = nodemailer.createTransport({
-      host: process.env.HOST,
-      service: process.env.SERVICE,
-      port: 587,
-      secure: true,
+      service: 'gmail',
       auth: {
+        type: 'OAuth2',
         user: process.env.USER,
-        pass: process.env.PASS
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET_ID
       }
     })
-
     await transporter.sendMail({
       from: process.env.USER,
       to: email,
